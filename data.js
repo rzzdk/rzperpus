5a1cae33125b6fb9d209c60224bafdb6c2d72008
rzzdk <rzzdkk@gmail.com>
2025-06-26 18:20:14 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,56 @@
+const koleksiBuku = [
+    {
+        judul: "Laskar Pelangi",
+        penulis: "Andrea Hirata",
+        tahunTerbit: 2005,
+        isbn: "979-3062-79-7",
+        deskripsi: "Kisah inspiratif tentang sepuluh anak dari keluarga miskin yang bersekolah di sebuah sekolah Muhammadiyah di Belitung.",
+        kategori: "Novel, Fiksi Indonesia",
+        gambar: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1589336932l/534575._SY475_.jpg"
+    },
+    {
+        judul: "Bumi Manusia",
+        penulis: "Pramoedya Ananta Toer",
+        tahunTerbit: 1980,
+        isbn: "978-979-97312-3-4",
+        deskripsi: "Bagian pertama dari Tetralogi Buru yang menceritakan perjalanan Minke di era kolonial Hindia Belanda.",
+        kategori: "Novel, Sejarah",
+        gambar: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1572928514l/17234._SY475_.jpg"
+    },
+    {
+        judul: "Sapiens: Riwayat Singkat Umat Manusia",
+        penulis: "Yuval Noah Harari",
+        tahunTerbit: 2011,
+        isbn: "978-602-424-416-3",
+        deskripsi: "Menjelajahi sejarah umat manusia dari zaman batu hingga era modern dengan sudut pandang yang unik.",
+        kategori: "Non-Fiksi, Sains Populer",
+        gambar: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1420585954l/23692271.jpg"
+    },
+    {
+        judul: "Atomic Habits",
+        penulis: "James Clear",
+        tahunTerbit: 2018,
+        isbn: "978-602-06-3317-6",
+        deskripsi: "Kerangka kerja praktis untuk meningkatkan diri setiap hari dengan membangun kebiasaan-kebiasaan kecil.",
+        kategori: "Pengembangan Diri",
+        gambar: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1655988385l/40121378.jpg"
+    },
+    {
+        judul: "Gadis Kretek",
+        penulis: "Ratih Kumala",
+        tahunTerbit: 2012,
+        isbn: "978-979-22-8141-5",
+        deskripsi: "Pencarian seorang perempuan misterius bernama Jeng Yah di industri kretek Jawa Tengah pada masa lampau.",
+        kategori: "Novel, Fiksi Sejarah",
+        gambar: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1330352536l/13524629.jpg"
+    },
+    {
+        judul: "rezaaa",
+        penulis: "rezaaaa",
+        tahunTerbit: 2012,
+        isbn: "978-979-22-8141-5",
+        deskripsi: "xe22242.",
+        kategori: "Novel, Fiksi Sejarah",
+        gambar: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1330352536l/13524629.jpg"
+    }
+];
